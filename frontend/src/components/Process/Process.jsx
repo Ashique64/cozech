4420a302f95d8d739a93461c901c57b29c1e8a5a
Ashique <ashique9496@gmail.com>
2025-10-01 18:33:52 +0530

    Process section completed

--- frontend/src/components/Process/Process.jsx
+++ frontend/src/components/Process/Process.jsx
@@ -46,15 +46,15 @@ const Process = () => {
                     </p>
                 </div>
 
-                <div className="card-section flex justify-center items-stretch gap-10 flex-wrap w-full">
+                <div className="card-section flex justify-center items-stretch gap-6 flex-wrap w-full">
                     {processData.map((item) => (
-                        <div key={item.id} className="card-wrapper">
+                        <div key={item.id} className="card-wrapper w-full md:w-[calc(50%-12px)] px-4 mb-8">
                             {item.hasAbstractDesign && (
                                 <div className="abstract-design">
                                     <img src="/images/card-layer.png" alt="" />
                                 </div>
                             )}
-                            <div className="card flex flex-col justify-center gap-4">
+                            <div className="card flex flex-col justify-center gap-4 h-full">
                                 <div className="item-1 flex justify-center items-center">
                                     <div className="logo">
                                         <img src={item.icon} alt={item.title} />
@@ -82,8 +82,8 @@ const Process = () => {
                                     xmlns="http://www.w3.org/2000/svg"
                                 >
                                     <path
-                                        fill-rule="evenodd"
-                                        clip-rule="evenodd"
+                                        fillRule="evenodd"
+                                        clipRule="evenodd"
                                         d="M0.333374 5.9999C0.333374 5.42 0.790044 4.9499 1.35337 4.9499H10.3809L7.4464 2.20678C7.04034 1.80484 7.02767 1.14015 7.41812 0.722139C7.80857 0.304129 8.45428 0.291096 8.86034 0.693029L13.6203 5.24303C13.8203 5.44099 13.9334 5.71428 13.9334 5.9999C13.9334 6.28552 13.8203 6.55881 13.6203 6.75678L8.86034 11.3068C8.45428 11.7087 7.80857 11.6957 7.41812 11.2777C7.02767 10.8597 7.04034 10.195 7.4464 9.79303L10.3809 7.0499H1.35337C0.790044 7.0499 0.333374 6.5798 0.333374 5.9999Z"
                                         fill="white"
                                     />
