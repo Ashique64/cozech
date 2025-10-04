60776a40f81942b5b07ae9e8621cd00cd2d3c4f6
Ashique <ashique9496@gmail.com>
2025-10-04 16:15:45 +0530

    CTA image blur added

--- frontend/src/components/CTA/CTA.jsx
+++ frontend/src/components/CTA/CTA.jsx
@@ -4,7 +4,7 @@ import "./CTA.scss";
 const CTA = () => {
     return (
         <section id="cta" className="cta w-full min-h-screen flex flex-col items-center justify-center md:p-20">
-            <div className="cta-container flex flex-col md:gap-16 justify-center items-center w-full ">
+            <div className="cta-container flex flex-col md:gap-6 justify-center items-center w-full ">
                 <img src="/images/CTA/svg/svgImage-1.svg" alt="Icon" className="left absolute top-0 left-0" />
                 <img src="/images/CTA/svg/svgImage-2.svg" alt="Icon" className="right absolute top-0 right-0" />
                 <div className="title-section w-full flex flex-col md:gap-12 p-4">
@@ -24,9 +24,10 @@ const CTA = () => {
                         </div>
                     </div>
                 </div>
-                {/* <div className="image-section">
-                    <div className="image-wrapper"><img src="/images/CTA/cta-image.png" alt="cta-image" /></div>
-                </div> */}
+                <div className="image-section w-full">
+                    <img src="/images/CTA/cta-image.png" alt="Decorative" className="cta-bottom-image" />
+                    <div className="bars w-full h-[180px] flex"></div>
+                </div>
             </div>
         </section>
     );
